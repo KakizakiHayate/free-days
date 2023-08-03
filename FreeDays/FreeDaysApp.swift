d8e8b93f1d94448e20034201f883e4e6821034d1
kakizaki hayate <hayate.h.0704@gmail.com>
2023-08-03 14:28:14 +0900

    プロジェクト作成

---
+++ FreeDays/FreeDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeDaysApp.swift
+//  FreeDays
+//
+//  Created by 柿崎逸 on 2023/08/03.
+//
+
+import SwiftUI
+
+@main
+struct FreeDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ScheduleSectionView()
+        }
+    }
+}
