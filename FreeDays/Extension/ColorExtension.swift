eaeb4766addd71fcec3d446be6bb05d74156c68f
kakizaki hayate <hayate.h.0704@gmail.com>
2023-08-03 15:55:42 +0900

    予定を追加するボタンと空いてる日を探すボタン用のcolorを追加

---
+++ FreeDays/Extension/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  FreeDays
+//
+//  Created by 柿崎逸 on 2023/08/03.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customColorAddButton = Color(red: 229 / 255, green: 124 / 255, blue: 35 / 255)
+    static let customColorFindFreeDaysButton = Color(red: 2 / 255, green: 84 / 255, blue: 100 / 255)
+}
