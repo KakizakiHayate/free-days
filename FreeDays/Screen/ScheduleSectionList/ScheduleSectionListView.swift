d8e58f87d9103aac2cfcea33d9e0b110ef14712d
kakizaki hayate <hayate.h.0704@gmail.com>
2023-08-03 15:56:47 +0900

    Listで予定を表示している部分を分割した

---
+++ FreeDays/Screen/ScheduleSectionList/ScheduleSectionListView.swift
@@ -0,0 +1,26 @@
+//
+//  ScheduleSectionListView.swift
+//  FreeDays
+//
+//  Created by 柿崎逸 on 2023/08/03.
+//
+
+import SwiftUI
+
+struct ScheduleSectionListView: View {
+    // MARK: - body
+    var body: some View {
+        List {
+            VStack {
+                Text("fff")
+            }
+        }
+    } // body
+} // view
+
+// MARK: - Preview
+struct ScheduleSectionListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleSectionListView()
+    }
+}
